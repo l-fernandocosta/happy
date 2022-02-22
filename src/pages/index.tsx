7f5611889745fffef050f81a0ab5ab3458080104
l-fernandocosta <fernandocostadev98@gmail.com>
2022-02-22 05:58:39 -0300

    input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
+import { Button, Stack, Flex } from "@chakra-ui/react"
+import {Input} from "../components/Input"
 
-
-export default function Home() {
+export default function SignIn() {
   return (
-    <div >
-        <h1>
-          Happy
-        </h1>
-    </div>
+    <Flex
+      align='center'
+      justify='center'
+      w='100vw'
+      h='100vh'>
+        <Flex as='form'
+          w='100%' maxW={360}
+          bg='gray.800'
+          p="8"
+          borderRadius={8}
+          flexDir='column'
+        >
+          <Stack spacing={"4"}>
+          <Input type={'email'} name= {'email'} label='E-mail'></Input>            
+          <Input type={'password'} name= {'Password'} label='Password'></Input>            
+
+
+            <Button colorScheme={"whatsapp"} type={"submit"} size="lg">Entrar</Button>
+              
+          </Stack>
+
+
+        </Flex>
+
+    </Flex>
   )
 }
