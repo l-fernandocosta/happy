ab5882a0b8bae674c8fbb96c54754fd10952028c
l-fernandocosta <fernandocostadev98@gmail.com>
2022-02-24 12:29:39 -0300

    Verification with yup

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,15 +1,25 @@
-import { FormControl, FormLabel, Input as ChakraInput , InputProps as ChakraInputProps} from "@chakra-ui/react";
+import { FormControl, FormErrorMessage, FormLabel, Input as ChakraInput , InputProps as ChakraInputProps, useToast} from "@chakra-ui/react";
+import {  forwardRef, ForwardRefRenderFunction } from "react";
+import {FieldError} from 'react-hook-form';
+
 
 interface InputProps  extends ChakraInputProps {
   name: string, 
   label?: string, 
+  error?: FieldError
 }
 
-export  function Input({name, label, ...rest} : InputProps) {
+
+const InputRef: ForwardRefRenderFunction<HTMLInputElement, InputProps> = 
+({name, label, error = null,...rest}, ref) => {
+
+  
   return(
-    <FormControl>
-      {!!label && <FormLabel htmlFor="email">{label}</FormLabel>}
+    <FormControl isInvalid = {!!error}>  
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      
       <ChakraInput
+        name= {name}
         id= {name}
         focusBorderColor={"green.500"}
         bg="gray.900"
@@ -18,10 +28,20 @@ export  function Input({name, label, ...rest} : InputProps) {
         _hover={{
           bgColor: 'gray.900'
         }}
+        ref ={ref}
         size='lg' 
         {...rest}
-        />
+        
+     />
+    {!!error  && (
+      
+      <FormErrorMessage>
+          {error.message}   
+      </FormErrorMessage>
+    )}
     </FormControl>
 
   )
 }
+
+export const Input = forwardRef(InputRef)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,37 @@
-import { Button, Stack, Flex } from "@chakra-ui/react"
-import { Input } from "../components/Input"
+import { Button, Stack, Flex, toast, useToast } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { Input } from "../components/Input";
+import {Rings} from 'react-loader-spinner';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
+import { useRouter } from "next/router";
+
+
+interface SignInData{
+  name: string,
+  password: string
+}
+
 
 export default function SignIn() {
+  const {push} = useRouter();
+  const schema = yup.object({
+    email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+    password: yup.string().required('You shall not pass!')
+  })
+
+
+
+  const {register, handleSubmit, formState: {errors, isSubmitting}} = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const handleSignIn = async (d: SignInData) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    
+    push('/dashboard')
+  }
+
   return (
     <Flex
       align='center'
@@ -14,12 +44,34 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir='column'
-      >
+        onSubmit={handleSubmit(handleSignIn)}
+        
+        >
         <Stack spacing={"4"}>
-          <Input type={'email'} name={'email'} label='E-mail' placeholder="darthdaver@outlook.com"></Input>
-          <Input type={'password'} name={'Password'} label='Password' placeholder="**********"></Input>
+          <Input
+            error={errors?.email}
+            type={'email'}
+            name={'email'}
+            label='E-mail'
+            placeholder="darthdaver@outlook.com"
+            {...register('email')}></Input>
+            
+          <Input
+            error={errors?.password}
+            type={'password'}
+            name={'Password'}
+            label='Password'
+            placeholder="**********"
+            {...register('password')}></Input>
 
-          <Button colorScheme={"whatsapp"} type={"submit"} size="lg">Entrar</Button>
+          <Button
+            spinner={<Rings color="#ffffff" height={70} width={70}/>}
+            colorScheme={"whatsapp"}
+            type={"submit"}
+            size="lg"
+            isLoading= {isSubmitting}
+            
+            >Entrar</Button>
 
         </Stack>
       </Flex>
